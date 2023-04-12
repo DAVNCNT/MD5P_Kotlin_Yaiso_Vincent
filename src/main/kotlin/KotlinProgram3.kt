9db1a7d7502821caef64e52ff3175584f33d9539
Vincent Yaiso <vryaiso@gmail.com>
2023-04-12 17:48:43 +0800

    Modified Kotlin Program 3

--- src/main/kotlin/KotlinProgram3.kt
+++ src/main/kotlin/KotlinProgram3.kt
@@ -1,5 +1,13 @@
 fun main(){
+
+    //For string input
     println("Enter your name")
     var name : String = readln()
     println("Hello $name!!")
+
+    //for integer input
+    println("enter your number")
+    var number : Int = readln().toInt()
+    println("You have entered the number $number")
+
 }
