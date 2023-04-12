145a9e416d983b4321e44927cc1928390e189015
Vincent Yaiso <vryaiso@gmail.com>
2023-04-12 15:27:45 +0800

    Kotlin Program 2

--- src/main/kotlin/KotlinProgram2.kt
+++ src/main/kotlin/KotlinProgram2.kt
@@ -1,3 +1,20 @@
 fun main(){
-    print("Welcome to Kodego")
+    println("Welcome to Kodego")
+
+    var name:String = "Vincent Yaiso"
+    println(name)
+    println("Hello $name")
+
+    name = "John Weak"
+    println("hello $name")
+
+    var age:Int =36
+    var salary:Double =1000.00
+    var skilllevel:Char='A'
+    var isActive:Boolean=true
+    println("His age is $age")
+    println("His Salaray is $salary")
+    println("His skill level is $skilllevel")
+    println("His curretly active? $isActive")
+
 }
