bdf201e69e0e804d5cdd82d6a4c48dde83d36fb3
Vincent Yaiso <vryaiso@gmail.com>
2023-04-12 21:02:21 +0800

    Kotlin Program 4

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,20 @@
+fun main(){
+    //arithmetic operators
+    println("This is an example of addition: ${2+5}")
+    // store in a variable
+    var sum: Int = 5+8
+    var difference: Int = 10-5
+    var product: Int = 8*5
+    var quotient: Int = 100/25
+    var modulo: Int = 5 % 2
+
+    println("the sum is $sum")
+    println("the difference is $difference")
+    println("the product is $product")
+    println("the quotient is $quotient")
+    println("the modulo is $modulo")
+
+    // 45 + 36 / 2 * 10
+    var computation = (45+36)/(2*10)
+    println("this is the result of the computation $computation")
+}
