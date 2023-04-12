e1b2537fce75a8d39032e235f71d691c0f6ac2bc
Vincent Yaiso <vryaiso@gmail.com>
2023-04-12 21:02:43 +0800

    Kotlin Program 5

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var lessThan = 5<2 //false
+    var greaterThan = 10>2 //true
+    var greaterThanorEqualTo = 10>=4 // true
+    var lessThanOrEqualTo = 5<=2 //false
+    var equalTo =1==1 //true
+    var notEqualTo = 1!=1 //false
+
+    println(lessThan)
+    println(greaterThan)
+    println(greaterThanorEqualTo)
+    println(lessThanOrEqualTo)
+    println(equalTo)
+    println(notEqualTo)
+
+
+
+
+
+}
