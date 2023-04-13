e862e3c9bdac21549d962d955c9b4c352d5bef22
Vincent Yaiso <vryaiso@gmail.com>
2023-04-13 21:23:56 +0800

    Practice Activity 1

--- src/main/kotlin/PracticeActivity1.kt
+++ src/main/kotlin/PracticeActivity1.kt
@@ -11,7 +11,7 @@ fun main(){
     //variables declaration
     var a = 2
     var b = 1
-    var computation2 = "${((a*2) + (2*a*b) - (4*(b+a))) / 2}"
+    var computation2 = "${((a*2) + (2*a*b) - 4*(b+a)) / 2}"
 
     println("The value of c is $computation2")
 }
