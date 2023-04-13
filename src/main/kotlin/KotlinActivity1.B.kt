be099995a5a89c20b4d961fc5ff1827babf5de6f
Vincent Yaiso <vryaiso@gmail.com>
2023-04-13 22:31:24 +0800

    Kotlin Activity 1.B

---
+++ src/main/kotlin/KotlinActivity1.B.kt
@@ -0,0 +1,24 @@
+fun main(){
+    //Things that can be borrowed in a library
+    //book
+    //artwork
+    //board Games
+    //maps
+    //films
+    //newspaper
+
+    println("Item Borrowed:")
+    var itemName = readln()
+
+    //Characteristic of all
+    println("\nIs the Item New?")
+    var ItemNew: String = readln()
+    println("\nIs the Item Old?")
+    var ItemOld: String =  readln()
+
+    println("\nFor Artwork and maps")
+    println("\nIs the Item New?")
+    var isItemBig: String = readln()
+    println("\nIs the Item Old?")
+    var isItemSmall: String = readln()
+}
