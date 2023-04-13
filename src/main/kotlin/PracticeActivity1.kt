a9f07f8d88e07530d37c43c9dcf0a82af29f8f0e
Vincent Yaiso <vryaiso@gmail.com>
2023-04-13 21:14:28 +0800

    Practice Activity 1

---
+++ src/main/kotlin/PracticeActivity1.kt
@@ -0,0 +1,17 @@
+fun main(){
+    // 1.1
+    //variables declaration
+    var base = 10
+    var height = 2
+    var computation = "${(base*height) / 2}"
+
+    println("The Area of the triangle is $computation")
+
+    //1.2
+    //variables declaration
+    var a = 2
+    var b = 1
+    var computation2 = "${((a*2) + (2*a*b) - (4*(b+a))) / 2}"
+
+    println("The value of c is $computation2")
+}
