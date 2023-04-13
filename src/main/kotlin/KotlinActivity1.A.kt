8606860f53e617fe6b58e8388ed98a7eedd644ac
Vincent Yaiso <vryaiso@gmail.com>
2023-04-13 22:15:19 +0800

    Kotlin Activity 1.A

---
+++ src/main/kotlin/KotlinActivity1.A.kt
@@ -0,0 +1,29 @@
+import jdk.dynalink.StandardNamespace
+
+fun main () {
+    //Activity 01-A
+    // Student's Directory
+    var name: String = "Vincent Yaiso"
+    var address: String = "Davao City"
+    var email: String = "vryaiso@gmail.com"
+    var number: Int = 1548632354
+    var facebook: String = "Vincent Yaiso"
+
+    println("\nStudent's Name: $name")
+    println("Student's Address: $address")
+    println("Student's Email: $email")
+    println("Student's Number: $number")
+    println("Student's Facebook: $facebook")
+
+
+
+    //Student's Progress
+    var testScore: Int = 100
+    var testMark: Char = 'A'
+    var isActivelyLearning: Boolean = true
+
+    println("\nStudent's Progress")
+    println("Student's Test Score: $testScore")
+    println("Student's Test Mark: $testMark")
+    println("The Student is Actively Learning? $isActivelyLearning")
+}
