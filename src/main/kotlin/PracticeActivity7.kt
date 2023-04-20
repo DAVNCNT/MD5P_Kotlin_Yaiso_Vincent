ce0aa93fcc296cdea5357081a7d10fa9b4ec7faf
Vincent Yaiso <vryaiso@gmail.com>
2023-04-20 21:41:50 +0800

    Practice Activity 7

---
+++ src/main/kotlin/PracticeActivity7.kt
@@ -0,0 +1,14 @@
+fun main(){
+    //variable
+    var num = 0
+
+    //Input
+    println("Enter a number: ")
+    num = readln().toInt()
+
+    if (num %2 == 0) {
+        println("The Number is Even")
+    }else{
+        println("The Number is Odd")
+    }
+}
