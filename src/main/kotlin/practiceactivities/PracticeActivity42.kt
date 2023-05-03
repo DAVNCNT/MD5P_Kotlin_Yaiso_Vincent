eb9ef3161d21703abb2afb597b312ea6df931873
Vincent Yaiso <vryaiso@gmail.com>
2023-05-03 18:56:40 +0800

    Practice Activity 42

--- src/main/kotlin/practiceactivities/PracticeActivity42.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity42.kt
@@ -1,11 +1,33 @@
 fun main(){
     //variables
     var string = ""
-    var char = 'A'
+    var character = 'a'
 
     //Input
     print("Enter a string: ")
     string = readln()
     print("Enter a character: ")
-    char = readln().first()
+    character = readln().first()
+
+    //Process - Output
+    var count = countOccurence(string,character)
+    if (count > 0) {
+        println("Character count: $count")
+    } else{
+        println("Character not found!!!")
+    }
+
+}
+
+fun countOccurence(text: String, char: Char):Int {
+    var textLength = text.length
+    var textSize = textLength - 1
+    var vowel = 0
+    for (ctrl in 0..textSize) {
+        if (text[ctrl] == char) {
+            vowel += 1
+        }
+    }
+    return vowel
+
 }
